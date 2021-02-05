666ef3861feab4a6bc2feab1f4c4f47bcf4178c9
eniliosarcos <eniliosarcos1993@gmail.com>
2021-02-05 15:58:46 -0400

    fin seccion 14

--- src/app/heroes/pages/agregar/agregar.component.ts
+++ src/app/heroes/pages/agregar/agregar.component.ts
@@ -1,16 +1,114 @@
 import { Component, OnInit } from '@angular/core';
+import { Heroe, Publisher } from '../../interfaces/heroes.interface';
+import { HeroesService } from '../../services/heroes.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import {switchMap} from 'rxjs/operators'
+import { ThrowStmt } from '@angular/compiler';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { MatDialog } from '@angular/material/dialog';
+import { ConfirmarComponent } from '../../components/confirmar/confirmar.component';
 
 @Component({
   selector: 'app-agregar',
   templateUrl: './agregar.component.html',
   styles: [
+    `
+      img{
+        width: 100%;
+        border-radius: 5px
+      }
+    `
   ]
 })
 export class AgregarComponent implements OnInit {
 
-  constructor() { }
+  heroe: Heroe = {
+    superhero:'',
+    alter_ego:'',
+    characters:'',
+    first_appearance:'',
+    publisher:Publisher.DCComics,
+    alt_img:'',
+  }
+
+  publishers = [
+    {
+      id:'DC Comics',
+      desc: 'DC - Comics'
+    },
+    {
+      id:'Marvel Comics',
+      desc: 'Marvel - Comics'
+    }
+  ]
+
+  constructor(private heroesService: HeroesService,
+    private activatedRoute: ActivatedRoute,
+    private router: Router,
+    private snackBar: MatSnackBar,
+    public dialog: MatDialog) { }
 
   ngOnInit(): void {
+
+    if(!this.router.url.includes("editar")){
+
+      return;  
+    }
+
+    this.activatedRoute.params
+    .pipe(
+      switchMap(({id})=>this.heroesService.getHeroeById(id))
+    )
+    .subscribe(heroe=>this.heroe = heroe);
+  }
+
+  guardar(){
+    if(this.heroe.superhero.trim().length === 0){
+
+      return;
+    }
+
+    if(this.heroe.id){
+      
+      this.heroesService.actualizarHeroe(this.heroe)
+        .subscribe(resp => this.mostrarSnackbar('Registro actualizado'));
+    }
+    else{
+
+      this.heroesService.agregarHeroe(this.heroe)
+      .subscribe(heroe => {
+
+        this.router.navigate(['/heroes/editar',heroe.id]);
+        this.mostrarSnackbar('Registro creado');
+      });
+    }
+  }
+
+  borrar(){
+
+    const dialog = this.dialog.open(ConfirmarComponent, {
+      width: '250px',
+      data: this.heroe
+    });
+
+    dialog.afterClosed().subscribe((result)=>{
+
+      if(result){
+
+        this.heroesService.borrarHeroe(this.heroe.id!)
+        .subscribe( resp => {
+          this.router.navigate(['/heroes']);
+        });
+      }
+    });
+
+  }
+
+  mostrarSnackbar(mensaje : string){
+
+    this.snackBar.open(mensaje,'ok!',{
+      duration: 2500
+    });
   }
 
 }
--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -10,7 +10,10 @@ import {MatCardModule} from '@angular/material/card';
 import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
 import {MatGridListModule} from '@angular/material/grid-list';
 import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatInputModule} from '@angular/material/input'
+import {MatInputModule} from '@angular/material/input';
+import {MatSelectModule} from '@angular/material/select';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
+import {MatDialogModule} from '@angular/material/dialog';
 
 
 
@@ -26,7 +29,10 @@ import {MatInputModule} from '@angular/material/input'
     MatGridListModule,
     MatAutocompleteModule,
     MatFormFieldModule,
-    MatInputModule
+    MatInputModule,
+    MatSelectModule,
+    MatSnackBarModule,
+    MatDialogModule
   ]
 })
 export class MaterialModule { }
